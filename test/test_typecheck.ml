967ffdee3a2375e80b8819114ae27ddff5fdc376
Pierre-Evariste Dagand <doc@evr.ist>
2025-10-09 15:52:13 +0200

    WIP: more test

--- test/test_typecheck.ml
+++ test/test_typecheck.ml
@@ -1,23 +1,28 @@
 open Alcotest
+open Ua0
 
-let x = Ua0.Ast.TermIdent.fresh "x"
-let y = Ua0.Ast.TermIdent.fresh "y"
-let z = Ua0.Ast.TermIdent.fresh "z"
-let env0 = failwith "NYI"
-let typs = failwith "NYI"
+let x = Ast.TermIdent.fresh "x"
+let y = Ast.TermIdent.fresh "y"
+let z = Ast.TermIdent.fresh "z"
+
+let env0 =
+  let open Uat.Typecheck.Env in
+  empty |> add_variable x Ast.Bool |> add_variable y Ast.Bool
+
+let typs = Alcotest.testable Ua0.Pp.pp_ty Util.Ty.equal
 
 let () =
   run "typesynth"
     [
       ( "var",
         [
-          test_case "var-in" `Quick (fun () ->
+          test_case "in" `Quick (fun () ->
               check typs "`x` has type `bool` in `env0`"
                 (Uat.Typecheck.typesynth env0 (Var x))
-                Ua0.Ast.TyBool);
-          test_case "var-in" `Quick (fun () ->
-              check typs "`x` has type `bool` in `env0`"
-                (Uat.Typecheck.typesynth env0 (Var x))
-                Ua0.Ast.TyBool);
+                Ua0.Ast.Bool);
+          test_case "unbound" `Quick (fun () ->
+              match_raises "`z` is not bound in `env0`"
+                (function Failure _ -> true | _ -> false)
+                (fun () -> ignore (Uat.Typecheck.typesynth env0 (Var z))));
         ] );
     ]
