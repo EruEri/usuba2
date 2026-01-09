ca1ee42efa0222e9ac2d07b520a8c0984b8e1f63
nayu <ndiayeyves@gmail.com>
2026-01-09 11:58:51 +0100

    pass - unfold

--- lib/unfold.ml
+++ lib/unfold.ml
@@ -1,4 +1,4 @@
-(** Unroll the let+ if the tuple contains a function pointer. We don't want to
+(** unfold the let+ if the tuple contains a function pointer. We don't want to
     unfold be a function is called on each element. *)
 
 module Env = struct
@@ -9,7 +9,7 @@ module Env = struct
   let arity _ _env : int = failwith ""
 end
 
-let rec f env term bindings constrs =
+let rec unfold' env term bindings constrs =
   match constrs with
   | [] ->
       List.fold_right
@@ -25,86 +25,95 @@ let rec f env term bindings constrs =
             (fun (variable, lterm) -> (variable, Term.Lookup { lterm; index }))
             bindings
         in
-        f env term bindings constrs
+        unfold' env term bindings constrs
       in
       Term.Constructor { ty = constr; terms }
 
-let unroll variable lterm ands prefix term =
+let unfold env variable lterm ands prefix term =
   let variable' = Ident.TermIdent.refresh "unfold" variable in
   let ands' =
     List.map
       (fun (variable, _) -> Ident.TermIdent.refresh "unfold" variable)
       ands
   in
-  let () = ignore (lterm, prefix, term, variable', ands') in
-  failwith ""
+  let bindings = (variable, lterm) :: ands in
+  let variables_fresh = variable' :: ands' in
+  let variables =
+    List.map2
+      (fun (variable, _) variable' -> (variable, Term.Var variable'))
+      bindings variables_fresh
+  in
+  let body = unfold' env term variables prefix in
+  List.fold_right2
+    (fun (_variable, lterm) variable' body ->
+      Term.Let { variable = variable'; term = lterm; k = body })
+    bindings variables_fresh body
 
-let rec cterm_unroll env = function
+let rec cterm_unfold env = function
   | Term.LetPlus { variable; lterm; ands; prefix; term } -> (
       (* TODO: we need the type of `term` *)
-      let lis, lterm = sterm_unroll env lterm in
+      let lis, lterm = sterm_unfold env lterm in
       let iss, ands =
         ands
         |> List.map (fun (variable, lterm) ->
-            let is, lterm = sterm_unroll env lterm in
+            let is, lterm = sterm_unfold env lterm in
             (is, (variable, lterm)))
         |> List.split
       in
-      let unroll = List.exists Fun.id (lis :: iss) in
+      let unfold = List.exists Fun.id (lis :: iss) in
       let () = ignore (variable, prefix, term, lterm, ands) in
-      match unroll with true -> failwith "" | false -> failwith "")
+      match unfold with true -> failwith "" | false -> failwith "")
   | Term.(True | False) as e -> (false, e)
   | Constructor { ty; terms } ->
       let contains, terms =
-        terms |> List.map (cterm_unroll env) |> List.split
+        terms |> List.map (cterm_unfold env) |> List.split
       in
       let contains = List.exists Fun.id contains in
       (contains, Constructor { ty; terms })
   | Let { variable; term; k } ->
-      let fn, term = sterm_unroll env term in
+      let fn, term = sterm_unfold env term in
       let env = Env.add variable fn env in
-      let fn, k = cterm_unroll env k in
+      let fn, k = cterm_unfold env k in
       (fn, Let { variable; term; k })
   | Log { message; variables; k } ->
-      let is, k = cterm_unroll env k in
+      let is, k = cterm_unfold env k in
       (is, Log { message; variables; k })
   | Synth sterm ->
-      let fn, sterm = sterm_unroll env sterm in
+      let fn, sterm = sterm_unfold env sterm in
       (fn, Synth sterm)
 
-and sterm_unroll env = function
+and sterm_unfold env = function
   | Term.Var variable as v ->
       let is = Env.find variable env in
       (is, v)
   | Fn _ as e -> (true, e)
   | Lookup { lterm; index } ->
-      let is, lterm = sterm_unroll env lterm in
+      let is, lterm = sterm_unfold env lterm in
       (is, Lookup { lterm; index })
   | Reindex { lhs; rhs; lterm } ->
-      let is, lterm = sterm_unroll env lterm in
+      let is, lterm = sterm_unfold env lterm in
       (is, Reindex { lhs; rhs; lterm })
   | Circ lterm ->
-      let is, lterm = sterm_unroll env lterm in
+      let is, lterm = sterm_unfold env lterm in
       (is, Circ lterm)
   | Lift { tys; func } ->
       (* is should be true *)
-      let is, func = sterm_unroll env func in
+      let is, func = sterm_unfold env func in
       (is, Lift { tys; func })
   | FnCall _ ->
       (* TODO: how to do without type info ? *)
       failwith ""
   | Operator operator ->
       let f (is, env) cterm =
-        let i, te = cterm_unroll env cterm in
+        let i, te = cterm_unfold env cterm in
         ((i || is, env), te)
       in
       let (is, _), operator = Operator.traverse f (false, env) operator in
       (is, Operator operator)
   | Ann (cterm, ty) ->
-      let is, cterm = cterm_unroll env cterm in
+      let is, cterm = cterm_unfold env cterm in
       (is, Ann (cterm, ty))
 
-let function_unroll _env function_decl =
-  let Prog.{ fn_name; signature; ops; args; body } = function_decl in
-  let () = ignore (fn_name, signature, ops, args, body) in
-  failwith ""
+let function_unfold env (function_decl : Prog.fndecl) =
+  let _, body = cterm_unfold env function_decl.body in
+  { function_decl with body }
